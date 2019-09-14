325210a690a54abc1999a31a765540549965eec4
Hector Javier <hjvm@mit.edu>
2019-09-14 15:11:56 -0400

    Initial Commit

---
+++ LinguaLens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinguaLens
+//
+//  Created by Hector Vazquez on 9/14/19.
+//  Copyright © 2019 Hector Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
